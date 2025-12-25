660c08149e261840ce6ef9eb7efc5d772a853bf4
Abhishree Gahlot <117459964+AbhishreeGahlot@users.noreply.github.com>
2025-12-25 11:46:45 +0530

    Update client.c

--- assignment-8/socketIPCMechanism/client/client.c
+++ assignment-8/socketIPCMechanism/client/client.c
@@ -3,35 +3,38 @@
 #include <unistd.h>
 
 int main() {
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in server;
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
 
-    server.sin_family = AF_INET;
-    server.sin_port = htons(8080);
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    struct sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8080);
+    serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    connect(sock, (struct sockaddr*)&server, sizeof(server));
+    connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
 
-    int choice, amount, result;
+    int userChoice;
+    int transactionAmount;
+    int serverResponse;
 
     printf("1. Withdraw\n2. Deposit\n3. Display Balance\n4. Exit\n");
-    scanf("%d", &choice);
+    scanf("%d", &userChoice);
 
-    write(sock, &choice, sizeof(choice));
+    write(clientSocket, &userChoice, sizeof(userChoice));
 
-    if (choice == 1 || choice == 2) {
+    if (userChoice == 1 || userChoice == 2) {
         printf("Enter amount: ");
-        scanf("%d", &amount);
-        write(sock, &amount, sizeof(amount));
+        scanf("%d", &transactionAmount);
+        write(clientSocket, &transactionAmount, sizeof(transactionAmount));
     }
 
-    if (choice != 4) {
-        read(sock, &result, sizeof(result));
-        if (result == -1)
+    if (userChoice != 4) {
+        read(clientSocket, &serverResponse, sizeof(serverResponse));
+        if (serverResponse == -1)
             printf("Insufficient Balance\n");
         else
-            printf("Current Balance: %d\n", result);
+            printf("Current Balance: %d\n", serverResponse);
     }
 
-    close(sock);
+    close(clientSocket);
+    return 0;
 }
